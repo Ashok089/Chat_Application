39b61b742faee98ea26c02f979b6f498af368e78
Ashok089 <148200587+Ashok089@users.noreply.github.com>
2025-03-29 13:43:40 +0530

    add message logic

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -17,6 +18,7 @@ app.use("/api/auth" , authRoutes)
 //  aa jyare User aa Auth Route par jase toh aa "authRoutes" will be called.  
 //  Suppose, user visit /api/auth/{signup or login or logout} it will redirect to page "authRoutes" where we written it's logic using "express". 
 
+app.use("/api/message" , messageRoutes)
 
 app.listen(PORT, () => {
     console.log("Server is running on PORT : " + PORT );
